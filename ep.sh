5c4ffe256e44839a42360bed74b3b9c3363d65d5
devubu <159164402+devubu@users.noreply.github.com>
2024-07-10 18:25:07 -0400

    Create ep.sh

---
+++ ep.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function display_help() {
+    echo "Usage: $0 [option]"
+    echo
+    echo "This script prints the contents of the /etc/proxychains4.conf file."
+    echo
+    echo "Options:"
+    echo "  --help, -help, -h    Display this help message and exit"
+}
+
+if [[ $1 == "--help" || $1 == "-help" || $1 == "-h" ]]; then
+    display_help
+    exit 0
+fi
+
+cat /etc/proxychains4.conf
